e4479e97a40528d07cf64ea73f808ccc4943f829
astern3 <avram.stern@digitas.com>
2021-01-04 10:02:58 -0600

    New component bloglist built, receiving props from Home

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,10 @@
 import { useState } from "react";
+import BlogList from "./BlogList";
 
 const Home = () => {
   const [blogs, setBlogs] = useState([
     {
-      title: "My new website oh",
+      title: "My new website oh yeah",
       body: "lorem ipsum...",
       author: "avram",
       id: 1,
@@ -17,19 +18,18 @@ const Home = () => {
     {
       title: "Residential services",
       body: "lorem ipsum...",
-      author: "nook",
+      author: "avram",
       id: 3,
     },
   ]);
 
   return (
     <div className="home">
-      {blogs.map((blog) => (
-        <div className="blog-preview" key={blog.id}>
-          <h2>{blog.title}</h2>
-          <p>Written by {blog.author}</p>
-        </div>
-      ))}
+      <BlogList blogs={blogs} title="All Blogs!" />
+      <BlogList
+        blogs={blogs.filter((blog) => blog.author === "avram")}
+        title="Avram's blogs"
+      />
     </div>
   );
 };
